1bdf68bf7245aea255afe2ae14ff02dd5fe1dd90
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-05-01 13:40:20 +0200

    Profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
   # Redirect root to the new action of ReservationsController
   root 'reservations#new'
-
-  # Other routes...
-
-  # Routes for taxi reservations
-  resources :reservations, only: [:new, :create, :show]
 end
