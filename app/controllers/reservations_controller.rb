a3ba09deba046a9b547cf1daa183ab8814af8bff
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-05-04 17:08:04 +0200

    Profile

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -10,19 +10,19 @@ class ReservationsController < ApplicationController
     end
   end
 
-  def create
-    @reservation = Reservation.new(reservation_params)
-    if @reservation.save
-      # Vous pouvez ajouter ici une logique pour notifier les chauffeurs disponibles ou choisir un chauffeur.
-      redirect_to reservation_path(@reservation), notice: 'Votre réservation a été créée avec succès. Nous recherchons un chauffeur pour vous.'
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
+  # def create
+  #   @reservation = Reservation.new(reservation_params)
+  #   if @reservation.save
+  #     # Vous pouvez ajouter ici une logique pour notifier les chauffeurs disponibles ou choisir un chauffeur.
+  #     redirect_to reservation_path(@reservation), notice: 'Votre réservation a été créée avec succès. Nous recherchons un chauffeur pour vous.'
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
 
-  def show
-    @reservation = Reservation.find(params[:id])
-  end
+  # def show
+  #   @reservation = Reservation.find(params[:id])
+  # end
 
   private
 
